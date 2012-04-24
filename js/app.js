03ea3e63734713ccf92414ba5e10096729e50028
Mahabubul Hasan Masud <masudiiuc@gmail.com>
2012-04-24 16:20:16 +0600

    new changes on app.js and images are added

--- js/app.js
+++ js/app.js
@@ -1,61 +1,68 @@
-(function($){
-    var Contacts = [
+(function ($) {
+
+    //demo data
+    var contacts = [
         { name: "Masud", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
         { name: "Himel", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
-        { name: "koushik", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "friend" },
+        { name: "Koushik", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "friend" },
         { name: "Monir", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "colleague" },
-        { name: "Fateha", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
-        { name: "Moinul", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "colleague" },
-        { name: "Zahid", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "friend" },
+        { name: "Jahid", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
+        { name: "Fateha", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "colleague" },
+        { name: "Emran", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "friend" },
         { name: "Mamoon", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" }
     ];
-}(jQuery));
 
-//model
-var Contact = Backbone.Model.extend({
-    defaults: {
-        photo: "/image/placeholder.png"
-    }
-});
+    //define product model
+    var Contact = Backbone.Model.extend({
+        defaults: {
+            photo: "/img/placeholder.png"
+        }
+    });
 
-//collection
-var Directory = Backbone.Collection.extend({
-    model: Contact
-});
+    //define directory collection
+    var Directory = Backbone.Collection.extend({
+        model: Contact
+    });
 
-//view
-var ContactView = Backbone.View.extend({
-    tagName: "article",
-    className: "contact-container",
-    template: $("#contact-template").html(),
+    //define individual contact view
+    var ContactView = Backbone.View.extend({
+        tagName: "article",
+        className: "contact-container",
+        template: $("#contactTemplate").html(),
 
-    render: function(){
-        var tmpl = _.template(this.template);
+        render: function () {
+            var tmpl = _.template(this.template);
 
-        this.$el.html(tmpl(this.model.toJSON()));
-        return this;
-    }
-});
+            $(this.el).html(tmpl(this.model.toJSON()));
+            return this;
+        }
+    });
 
+    //define master view
+    var DirectoryView = Backbone.View.extend({
+        el: $("#contacts"),
 
-//master view
+        initialize: function () {
+            this.collection = new Directory(contacts);
+            this.render();
+        },
 
-var DirectoryView = Backbone.View.extend({
-    el: $("#contacts"),
-    initialize: function(){
-        this.collection = new Directory(Contacts);
-        this.render();
-    },
-    render: function(){
-        var that = this;
-        _.each(this.collection.models, function(item){
-            that.renderContact(item);
-        },this);
-    },
-    renderContact: function(){
-        var ContactView  = new ContactView({
-            model: item
-        });
-        this.$el.append(ContactView.render().el);
-    }
-});
+        render: function () {
+            var that = this;
+            _.each(this.collection.models, function (item) {
+                that.renderContact(item);
+            }, this);
+        },
+
+        renderContact: function (item) {
+            var contactView = new ContactView({
+                model: item
+            });
+            this.$el.append(contactView.render().el);
+        }
+    });
+
+    //create instance of master view
+    var directory = new DirectoryView();
+
+} (jQuery));
